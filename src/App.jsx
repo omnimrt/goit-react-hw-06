5ff2d543505263fefb6a64c76bbca37b44f76fc5
Martin Myhalyna <martinmyhalyna@gmail.com>
2024-04-25 12:37:41 +0200

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import "./App.css";
+import ContactList from "./components/ContactList/ContactList";
+import userData from "./users.json";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
+import { nanoid } from "nanoid";
+import { useSelector } from "react-redux";
+import {
+  addContact,
+  deleteContact,
+  setFilter,
+} from "./redux/phonebook/phonebookReducer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+  // const [users, setUsers] = useState(() => {
+  //   const stringifiedUsers = localStorage.getItem("users");
+  //   if (!stringifiedUsers) return userData;
+
+  //   const parsedUsers = JSON.parse(stringifiedUsers);
+  //   return parsedUsers;
+  // });
+
+  // const [filter, setFilter] = useState("");
+
+  const contacts = useSelector((state) => state.phonebook.contacts);
+  const filters = useSelector((state) => state.phonebook.filter);
+
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(users));
+  }, [users]);
+
+  // User adding functionality
+
+  const onAddUser = (formData) => {
+    const finalUser = {
+      ...formData,
+      id: nanoid(),
+    };
+    dispatch(addContact(finalUser));
+  };
+
+  // User deleting functionality
+
+  const onUserDelete = (userId) => {
+    dispatch(deleteContact(userId));
+  };
+
+  // Filter functionality
+
+  const onChangeFilter = (event) => {
+    dispatch(setFilter(event.target.value));
+  };
+
+  const filteredUsers = users.filter((user) => {
+    const nameIncludesFilter =
+      user.name && user.name.toLowerCase().includes(filter.toLowerCase());
+    const numberIncludesFilter =
+      typeof user.number === "string" &&
+      user.number.toLowerCase().includes(filter.toLowerCase());
+    return nameIncludesFilter || numberIncludesFilter;
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Phonebook</h1>
+      <br />
+      <ContactForm onAddUser={onAddUser} />
+      <SearchBox onChangeFilter={onChangeFilter} value={filter} />
+      <ContactList users={filteredUsers} onUserDelete={onUserDelete} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
